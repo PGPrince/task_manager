7ed0cc8efd119ba5340d96f3caf3b4631bf9cbc1
Prince Ghosh <ghoshprince999@gmail.com>
2025-06-23 11:29:22 +0600

    task manager

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:task_management/splash_screen.dart';
+import 'package:task_management/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,62 +9,36 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Task Manager',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Splash(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      home: AnimatedSplashScreen(
+        duration: 3000,
+        splashTransition: SplashTransition.fadeTransition,
+        backgroundColor: Colors.transparent,
+        nextScreen: MyHomePage(title: "Task Manager"),
+        splashIconSize: double.infinity,
+        splash: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromRGBO(156, 44, 243, 1),
+                Color.fromRGBO(58, 73, 249, 1),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
-          ],
+          ),
+          child: const Center(
+            child: Icon(Icons.task, size: 300, color: Colors.white),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,16 +1,47 @@
 import 'package:flutter/material.dart';
 
-class Splash extends StatelessWidget {
-  const ({Key? key}) : super(key:key);
+import 'home.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    //_navigatehome();
+  }
+
+  _navigatehome() async {
+    await Future.delayed(Duration(microseconds: 1500), () {});
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => MyHomePage(title: "Task Manager"),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text("Splash Screen", style: TextStyle(
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-        ),),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(height: 100, width: 100, color: Colors.blue),
+            Container(
+              child: Text(
+                "Splash Screen",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
